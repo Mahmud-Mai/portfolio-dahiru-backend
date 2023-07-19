fd0e265a7bb370a9c135f31a00605890ba5b4c7e
Mahmud Mai <mahmudx00@gmail.com>
2023-07-19 08:41:10 +0100

    define projects document

---
+++ schemas/projects.js
@@ -0,0 +1,34 @@
+export default {
+  name: 'projects',
+  title: 'Projects',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'imgUrl',
+      title: 'ImageUrl',
+      type: 'image',
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [
+        {
+          name: 'tag',
+          title: 'Tag',
+          type: 'string',
+        },
+      ],
+    },
+  ],
+}
