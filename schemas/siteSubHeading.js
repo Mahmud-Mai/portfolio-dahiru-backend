c97abd35e43b623a1b938b75d3e1943166577429
Mahmud Mai <mahmudx00@gmail.com>
2023-07-19 08:42:01 +0100

    define siteSubHeading document

---
+++ schemas/siteSubHeading.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'siteSubHeading',
+  title: 'Site Sub Heading',
+  type: 'document',
+  fields: [
+    {
+      name: 'subHeading',
+      title: 'Sub Heading',
+      type: 'string',
+    },
+  ],
+}
